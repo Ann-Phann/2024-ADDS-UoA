2c2ee2ef215207ff67e051ba406671c17ebd634a
An Phan <tqtthuyan71@gmail.com>
2024-08-22 13:46:08 +0930

    just the question

--- Finder.cpp
+++ Finder.cpp
@@ -2,23 +2,45 @@
 
     using namespace std;
 
-    vector<int> Finder::findSubstrings(string s1, string s2) {
+    //reduce the number of iterations by using the last found index
+    // vector<int> Finder::findSubstrings(string s1, string s2) {
+
+    //     vector<int> result;
+    //     size_t last_found = 0;
+
+    //     for(size_t i = 1; i <= s2.size(); i++) {
+    //         size_t found = s1.find(s2.substr(0, i), last_found);
+    //         if (found != string::npos) {
+    //             result.push_back(found);
+    //         } else {
+    //             result.push_back(-1);
+    //         }
+    //     }
+    //     while (result.size() < s2.size())
+    //     {
+    //         result.push_back(-1);
+    //     }
+    //     return result;
+    // }
+
+
+
+        vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-        size_t last_found = 0;
 
         for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i), last_found);
+            size_t found = s1.find(s2.substr(0,i));
             if (found != string::npos) {
                 result.push_back(found);
             } else {
                 result.push_back(-1);
             }
         }
-        while (result.size() < s2.size())
-        {
-            result.push_back(-1);
-        }
+        // while (result.size() < s2.size())
+        // {
+        //     result.push_back(-1);
+        // }
         return result;
     }
 
