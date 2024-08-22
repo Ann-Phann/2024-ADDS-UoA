0902e8f0d59b9f1757fe6b1a636a951c090adba7
An Phan <tqtthuyan71@gmail.com>
2024-08-22 14:27:18 +0930

    new prac4

--- Finder.cpp
+++ Finder.cpp
@@ -5,25 +5,26 @@
     //reduce the number of iterations by using the last found index
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
-        vector<int> result;
+        
+        vector<int> result (s2.size(), -1);
         size_t last_found = 0;
 
         for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i), last_found);
-            if (found != string::npos) {
-                //result[i-1] = found;
-                result.push_back(found);
-                last_found = found;
+            last_found = s1.find(s2.substr(0, i), last_found);
+            if (last_found != string::npos) {
+                result[i-1] = last_found;
+                //result.push_back(found);
+                //last_found = found;
             } 
-            // else {
-            //     result.push_back(-1);
-            //     break;
-            // }
+            else {
+                //result.push_back(-1);
+                break;
+            }
         }
-        while (result.size() < s2.size())
-        {
-            result.push_back(-1);
-        }
+        // while (result.size() < s2.size())
+        // {
+        //     result.push_back(-1);
+        // }
         return result;
     }
    
