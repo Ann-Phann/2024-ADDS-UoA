c308b5e089af1df592853b74ee9309d3dcdb911c
An Phan <tqtthuyan71@gmail.com>
2024-10-04 10:16:32 +0930

    prac7 q1

--- Autocomplete.h
+++ Autocomplete.h
@@ -16,7 +16,7 @@ private:
     Node* createNewNode();
 
     // Helper function to recursively collect all words from a given node
-    void searchPrefix(Node* node, std::string prefix, std::vector<std::string>result);
+    void searchPrefix(Node* node, std::string prefix, std::vector<std::string>& result);
 public:
     Autocomplete();
     std::vector<std::string> getSuggestions(std::string partialWord); // return the known words that start with partialWord
